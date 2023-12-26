75f27f3cdf4370de402b50c5cc682751100e7991
GGNE <z_zzlzl@naver.com>
2023-12-26 20:56:28 +0900

    심볼, Object Type, Array

--- 1_basics/4_data_types.js
+++ 1_basics/4_data_types.js
@@ -109,5 +109,73 @@ let init = null;
 console.log(init);
 // null이 object 타입으로 확인 되는 버그가 있다. (JS 개발자가 인정함.)
 console.log(typeof init);
+console.log('=================');
 
 
+/**
+ * Symbol 
+ * 
+ * 유일무이한 값을 생성할 때 사용한다. 
+ * 다른 프리미티브 값들과 다르게 Symbol 함수를 호출해서 사용한다. 
+ */
+const test1 = '1';
+const test2 = '1';
+
+console.log(test1 === test2);       // true
+
+// 유일한 변수값이 된다.
+const symbol1 = Symbol('1');
+const symbol2 = Symbol('1');
+
+console.log(symbol1 === symbol2);   // false
+console.log('=================');
+
+
+/**
+ * Object 타입 
+ * 
+ * key와 value의 쌍으로 이루어져 있다. 
+ * ex) key : value
+ */
+const dictionary = {
+    red : '빨간색',
+    orange : '주황색',
+    yellow : '노오오란색'
+};
+
+console.log(dictionary);
+console.log(dictionary['red']);     // search by key
+console.log(typeof dictionary);
+
+
+/**
+ * Array 타입
+ * 
+ * 값을 리스트로 나열할 수 있는 타입이다. 
+ */
+
+// 0번부터 순서대로 인덱스 할당 됨.
+const iveMemberArr = [
+    '안유진',
+    '가을',
+    '레이',
+    '장원영',
+    '리즈',
+    '이서'
+];
+
+console.log(iveMemberArr);
+console.log(iveMemberArr[2]);       // 레이
+iveMemberArr[5] = 'SJH';
+console.log(iveMemberArr);
+console.log(typeof iveMemberArr);   // Array도 typeof로는 object
+
+
+/**
+ * static typing : 변수를 선언할 때, 어떤 타입의 변수를 선언할지 명시 
+ * ex) C
+ * 
+ * dynamic typing : 변수의 타입을 명시적으로 선언하지 않고 타입을 추론한다.
+ * ex) JavaScript
+ * 
+ */
